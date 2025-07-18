477716d99e811252fbbf366e12d573ef7ba2077d
ABOK451 <alancito2013america@gmail.com>
2025-07-17 18:31:01 -0600

    cambios en el package json y en el index

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const userRoutes = require('./routes/user.routes');
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor en http://localhost:${port}`);
